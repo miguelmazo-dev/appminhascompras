f7a062eb76543a5eb23d0c6a93f62b3720d037cb
miguel henrique mazo <miguelmazo@myyahoo.com>
2026-03-22 17:47:03 -0300

    coisa boa

--- appminhascompras/Views/EditarProduto.xaml.cs
+++ appminhascompras/Views/EditarProduto.xaml.cs
@@ -1,3 +1,5 @@
+using appminhascompras.Models;
+
 namespace appminhascompras.Views;
 
 public partial class EditarProduto : ContentPage
@@ -6,4 +8,34 @@ public partial class EditarProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+
+        {
+
+            Produto produto_anexado = BindingContext as Produto;
+
+            Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                Quantidade = double.Parse(txt_quantidade.Text),
+                Preco = double.Parse(txt_preco.Text)
+
+            };
+
+            await App.Db.Update(p);
+            await DisplayAlert("Sucesso!", "Registro Atualizado", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("ops", ex.Message, "ok");
+
+
+
+        }
+    }
 }
